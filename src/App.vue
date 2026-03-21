1aa66fe8ea44b794082a6f4c9e00bfa158ea016f
KerishT <kerish.turista@gmail.com>
2026-03-21 08:49:46 +0500

    Add Card, IconClose, IconTick

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,14 @@
 <script setup>
+  import Card from "./components/Card.vue"
   import Header from "./components/Header.vue"
 </script>
 
 <template>
   <Header />
 
-  <div class="content"></div>
+  <div class="content">
+    <Card :index="1" :label="'Test'" />
+  </div>
 </template>
 
 <style scoped></style>
