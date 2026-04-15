c3ab6692fec8f48140e212a8b58267882300179c
KerishT <kerish.turista@gmail.com>
2026-04-15 21:06:48 +0500

    Add closed and status processing, add Cards

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,51 @@
 <script setup>
   import { ref } from "vue"
-  import Card from "./components/Card.vue"
+  import Cards from "./components/Cards.vue"
   import Header from "./components/Header.vue"
 
   const score = ref(10)
-  const cardData = ref({
-    index: 1,
-    word: "Test",
-    translation: "Тест",
-    state: "opened",
-    status: "success",
-  })
+  const cards = ref([
+    {
+      index: 1,
+      word: "Test",
+      translation: "Тест",
+      state: "closed",
+      status: "pending",
+    },
+    {
+      index: 2,
+      word: "Test",
+      translation: "Тест",
+      state: "opened",
+      status: "pending",
+    },
+    {
+      index: 3,
+      word: "Test",
+      translation: "Тест",
+      state: "closed",
+      status: "success",
+    },
+    {
+      index: 4,
+      word: "Test",
+      translation: "Тест",
+      state: "closed",
+      status: "error",
+    },
+  ])
 </script>
 
 <template>
   <Header :score />
 
-  <div class="content">
-    <Card v-bind="cardData" />
-  </div>
+  <main class="content">
+    <Cards :cards="cards" />
+  </main>
 </template>
 
-<style scoped></style>
+<style scoped>
+  .content {
+    padding: 0 30px 40px 30px;
+  }
+</style>
